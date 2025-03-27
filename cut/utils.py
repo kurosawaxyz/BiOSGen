fb7ac7dd177236b5e1cfdf1e974b7048e588d62b
Duong <vuhoang.thduong@gmail.com>
2025-03-27 20:25:33 +0100

    update cut

--- cut/utils.py
+++ cut/utils.py
@@ -1,28 +1,7 @@
 import torch
-import yaml
 import pandas as pd
 import numpy as np
 from PIL import Image
-
-class DotDict:
-    """Recursively convert a nested dictionary into an object supporting dot notation."""
-    def __init__(self, dictionary):
-        for key, value in dictionary.items():
-            if isinstance(value, dict):
-                value = DotDict(value)  # Recursively convert sub-dictionaries
-            setattr(self, key, value)
-
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def __repr__(self):
-        return str(self.__dict__)
-
-
-def load_config(config_path):
-    with open(config_path, "r") as f:
-        args = yaml.safe_load(f)  # Load as dictionary
-    return DotDict(args)  # Convert to dot notation
 
 def load_data(data_csv_path, batch_size):
     data = pd.read_csv(data_csv_path)[:batch_size]
