d1ee72c9e7a4738a538db601824bdee54e294941
duong <vuhoang.thduong@gmail.com>
2025-04-17 14:33:39 +0200

    update v2, delete unnecessary files

--- cyclegan/test.sh
+++
@@ -1,8 +0,0 @@
-# Copy the checkpoint file
-cp pytorch-CycleGAN-and-pix2pix/checkpoints/horse2zebra/latest_net_G_A.pth checkpoints/horse2zebra/latest_net_G.pth
-
-# Run the test script
-python pytorch-CycleGAN-and-pix2pix/test.py --dataroot cyclegan/data/testA --name tumor_cyclegan --model test --no_dropout && \
-
-# Print a message when testing is finished
-echo "Testing finished."
