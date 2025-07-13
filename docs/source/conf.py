8c62e3a205edb9eea62b860c328a01b16c4d07a8
VivienneForReal <vuhoang.thduong@gmail.com>
2025-07-13 11:41:53 +0200

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,31 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+from unittest.mock import MagicMock
+
+sys.path.insert(0, os.path.abspath('..'))  # so Sphinx can find your package
+
+project = 'BiOSGen'
+author = 'H. T. Duong Vu'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',        # For Google/NumPy-style docstrings
+    'sphinx.ext.viewcode',        # Adds links to highlighted source code
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+MOCK_MODULES = [
+    "torch", "torchvision", "torchaudio",
+    "flash_attn", "clip", "transformers", "diffusers",
+    "loralib", "peft", "pytorch_fid", "cleanfid",
+    "thop", "torchviz", "torchinfo"
+]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = MagicMock()
